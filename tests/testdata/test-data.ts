dd04dcda7a19b3f8d0ccfb6b0f32df9663902735
Yudi <yadvendra.singh@coredge.com>
2025-02-06 10:56:26 +0530

    Adding test data file

---
+++ tests/testdata/test-data.ts
@@ -0,0 +1,19 @@
+export const successLoginCredentials = {
+  username: 'standard_user',
+  password: 'secret_sauce',
+};
+
+export const failureLoginCredentials = {
+  username: 'standard_user',
+  password: '123',
+};
+///fin
+export const successOnBoardingLoginCredentials = {
+  username: 'josephsinha@yopmail.com',
+  password: 'Qwertyuiop@1',
+};
+
+export const failureOnBoardingLoginCredentials = {
+  username: 'osephsinha@yopmail.com',
+  password: 'Qwertyuiop@1123',
+};
