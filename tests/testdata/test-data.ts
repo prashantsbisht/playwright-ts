e02124f445d7a74c44ef28a6988fb526b1f169f3
Yudi <yadvendra.singh@coredge.com>
2025-02-06 13:07:31 +0530

    Adding test data file

--- tests/testdata/test-data.ts
+++ tests/testdata/test-data.ts
@@ -1,13 +1,3 @@
-export const successLoginCredentials = {
-  username: 'standard_user',
-  password: 'secret_sauce',
-};
-
-export const failureLoginCredentials = {
-  username: 'standard_user',
-  password: '123',
-};
-///fin
 export const successOnBoardingLoginCredentials = {
   username: 'josephsinha@yopmail.com',
   password: 'Qwertyuiop@1',
